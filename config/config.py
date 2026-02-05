0ba67828514d3c4352428a5b3cd6ad711946d2bd
mayuri swami <mayuriswami48@gmail.com>
2026-02-05 05:57:33 +0000

    first commit

---
+++ config/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+HF_TOKEN = os.getenv("HF_TOKEN")
+MODEL_NAME = "groq:qwen/qwen3-32b"
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from animeee!")
+
+
+if __name__ == "__main__":
+    main()
